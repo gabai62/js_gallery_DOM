4cd3c3d056fd79e5c7d5d48b0ebb78c41ab1de08
gabai62 <gabai62it@gmail.com>
2025-04-08 13:19:12 +0300

    make build

---
+++ index.556ab8e8.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var a=t.target;if("IMG"===a.tagName||"A"===a.tagName){var n=a.closest("a");n&&(e.src=n.href)}});
+//# sourceMappingURL=index.556ab8e8.js.map
